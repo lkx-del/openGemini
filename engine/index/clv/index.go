9bc1fa4366ce3ca2d5508d9e3bb630f196c9c0a4
lijieac <lijieac@outlook.com>
2023-03-25 11:03:25 +0800

    feat(TextIndex): add the support for merger.
    Signed-off-by: lijieac <lijieac@outlook.com>

--- engine/index/clv/index.go
+++ engine/index/clv/index.go
@@ -19,12 +19,10 @@ package clv
 import (
 	"fmt"
 	"path"
-	"sort"
 	"strings"
 	"sync"
 	"time"
 
-	"github.com/VictoriaMetrics/VictoriaMetrics/lib/encoding"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/mergeset"
 	"github.com/openGemini/openGemini/engine/index/mergeindex"
 )
@@ -36,43 +34,52 @@ const (
 	txPrefixDicVersion
 	txPrefixSid
 	txPrefixId
+	txPrefixMeta
 	txSuffix = 9
 )
 
 const (
-	qmin        = 1
-	posFlag     = 1
-	idFlag      = 2
-	maxBuf      = 50000
-	maxItemSize = 64 * 1024
+	qmin   = 1
+	maxBuf = 2000 // can not to large, otherwise AddItems may fail because the item size is greater than 64k.
 )
 
 type InvertState struct {
 	timestamp int64
-	pos       uint16
+	position  uint16
 }
 
-type InvertedIndex struct {
-	iverteState []InvertState
+type InvertStates struct {
+	invertState []InvertState
+	tsid        uint64
 }
 
-func NewInvertedIndex() *InvertedIndex {
-	return &InvertedIndex{
-		iverteState: make([]InvertState, 0),
+func NewInvertStates() *InvertStates {
+	return &InvertStates{
+		invertState: make([]InvertState, 0),
+	}
+}
+
+type InvertIndex struct {
+	invertStates map[uint64]*InvertStates // tsid is the key.
+	ids          map[uint32]struct{}
+}
+
+func NewInvertIndex() InvertIndex {
+	return InvertIndex{
+		invertStates: make(map[uint64]*InvertStates),
+		ids:          make(map[uint32]struct{}),
 	}
 }
 
 type TrieNode struct {
-	children      map[string]*TrieNode
-	invertedIndex map[uint64]*InvertedIndex
-	ids           map[uint32]struct{}
+	children    map[string]*TrieNode
+	invertIndex InvertIndex
 }
 
 func NewTrieNode() *TrieNode {
 	return &TrieNode{
-		children:      make(map[string]*TrieNode),
-		invertedIndex: make(map[uint64]*InvertedIndex),
-		ids:           make(map[uint32]struct{}),
+		children:    make(map[string]*TrieNode),
+		invertIndex: NewInvertIndex(),
 	}
 }
 
@@ -88,7 +95,6 @@ type TokenIndex struct {
 	analyzer    *Analyzer
 	trieLock    sync.RWMutex
 	closing     chan struct{}
-	signal      chan struct{}
 	path        string
 	measurement string
 	field       string
@@ -99,7 +105,6 @@ func NewTokenIndex(opts *Options) (*TokenIndex, error) {
 	idx := &TokenIndex{
 		root:        NewTrieNode(),
 		closing:     make(chan struct{}),
-		signal:      make(chan struct{}),
 		path:        opts.Path,
 		measurement: opts.Measurement,
 		field:       opts.Field,
@@ -140,7 +145,7 @@ func NewTokenIndex(opts *Options) (*TokenIndex, error) {
 
 func (idx *TokenIndex) Open() error {
 	tbPath := path.Join(idx.path, idx.measurement, idx.field)
-	tb, err := mergeset.OpenTable(tbPath, nil, nil)
+	tb, err := mergeset.OpenTable(tbPath, nil, mergeDocIdxItems)
 	if err != nil {
 		return fmt.Errorf("cannot open text index:%s, err: %+v", tbPath, err)
 	}
@@ -153,9 +158,6 @@ func (idx *TokenIndex) Open() error {
 }
 
 func (idx *TokenIndex) Close() error {
-	if idx.signal != nil {
-		close(idx.signal)
-	}
 	if idx.closing != nil {
 		idx.closing <- struct{}{}
 		close(idx.closing)
@@ -164,17 +166,16 @@ func (idx *TokenIndex) Close() error {
 }
 
 func (idx *TokenIndex) insertInvertedIndex(node *TrieNode, tsid uint64, timestamp int64, position uint16) {
-	invertedIndex, ok := node.invertedIndex[tsid]
+	invertedIndex, ok := node.invertIndex.invertStates[tsid]
 	if !ok {
-		invertedIndex = NewInvertedIndex()
-		node.invertedIndex[tsid] = invertedIndex
+		invertedIndex = NewInvertStates()
+		invertedIndex.tsid = tsid
+		node.invertIndex.invertStates[tsid] = invertedIndex
 	}
-
-	state := InvertState{timestamp, position}
-	invertedIndex.iverteState = append(invertedIndex.iverteState, state)
+	invertedIndex.invertState = append(invertedIndex.invertState, InvertState{timestamp, position})
 }
 
-func (idx *TokenIndex) insertTrieNode(vtoken []string, tsid uint64, timestamp int64, position uint16) error {
+func (idx *TokenIndex) insertTrieNode(vtoken []string, tsid uint64, timestamp int64, position uint16) {
 	idx.trieLock.Lock()
 	idx.docNum++
 	node := idx.root
@@ -186,13 +187,11 @@ func (idx *TokenIndex) insertTrieNode(vtoken []string, tsid uint64, timestamp in
 		}
 		node = child
 	}
-	idx.trieLock.Unlock()
-
 	idx.insertInvertedIndex(node, tsid, timestamp, position)
-	return nil
+	idx.trieLock.Unlock()
 }
 
-func (idx *TokenIndex) insertSuffixToTrie(vtoken []string, id uint32) error {
+func (idx *TokenIndex) insertSuffixToTrie(vtoken []string, id uint32) {
 	idx.trieLock.Lock()
 	node := idx.root
 	for _, token := range vtoken {
@@ -204,12 +203,74 @@ func (idx *TokenIndex) insertSuffixToTrie(vtoken []string, id uint32) error {
 		node = child
 	}
 
-	if _, ok := node.ids[id]; !ok {
-		node.ids[id] = struct{}{}
+	if _, ok := node.invertIndex.ids[id]; !ok {
+		node.invertIndex.ids[id] = struct{}{}
 	}
 	idx.trieLock.Unlock()
 
-	return nil
+	return
+}
+
+var idxItemsPool mergeindex.IndexItemsPool
+
+func (idx *TokenIndex) writeDicVersion(version uint32) {
+	return
+}
+
+func (idx *TokenIndex) createIndex(vtoken string, node *TrieNode) error {
+	if len(node.invertIndex.invertStates) == 0 && len(node.invertIndex.ids) == 0 {
+		return nil
+	}
+
+	ii := idxItemsPool.Get()
+	defer idxItemsPool.Put(ii)
+
+	ii.B = marshal(ii.B, vtoken, &node.invertIndex)
+	ii.Next()
+	// write to mergeset
+	return idx.tb.AddItems(ii.Items)
+}
+
+func (idx *TokenIndex) writeDocumentIndex(vtoken string, node *TrieNode) {
+	idx.createIndex(vtoken, node)
+	// traversal of trees.
+	for token, child := range node.children {
+		vtokens := vtoken + token + " "
+		idx.writeDocumentIndex(vtokens, child)
+	}
+}
+
+func (idx *TokenIndex) processDocument() {
+	// replace the root node
+	idx.trieLock.Lock()
+	if len(idx.root.children) == 0 {
+		idx.trieLock.Unlock()
+		return
+	}
+	node := idx.root
+	idx.docNum = 0
+	idx.root = NewTrieNode()
+	idx.trieLock.Unlock()
+
+	// Deal the first level node of the tree.
+	for token, child := range node.children {
+		vtokens := token + " "
+		idx.writeDocumentIndex(vtokens, child)
+	}
+}
+
+func (idx *TokenIndex) process() {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-idx.closing:
+			return
+		case <-ticker.C:
+			idx.processDocument()
+		}
+	}
 }
 
 func (idx *TokenIndex) AddDocument(log string, tsid uint64, timestamp int64) error {
@@ -235,135 +296,10 @@ func (idx *TokenIndex) AddDocument(log string, tsid uint64, timestamp int64) err
 	}
 
 	if idx.docNum >= maxBuf {
-		idx.signal <- struct{}{}
+		idx.processDocument()
 	}
 
 	return nil
-}
-
-var idxItemsPool mergeindex.IndexItemsPool
-
-func (idx *TokenIndex) writeDicVersion(version uint32) {
-	return
-}
-
-func (idx *TokenIndex) marshalPosList(dst []byte, node *TrieNode) []byte {
-	// len(sid)
-	dst = encoding.MarshalUint32(dst, uint32(len(node.invertedIndex)))
-
-	for sid, iindex := range node.invertedIndex {
-		// prefixSid + sid + len(pos) + timelist + positionlist
-		// sort by timestamp
-		istate := iindex.iverteState
-		sort.Slice(istate, func(i, j int) bool {
-			return istate[i].timestamp < istate[j].timestamp
-		})
-		dst = append(dst, txPrefixSid)
-		dst = encoding.MarshalVarUint64(dst, sid)
-		dst = encoding.MarshalUint32(dst, uint32(len(istate)))
-		posbuffer := make([]byte, 2*len(istate))
-		for i := 0; i < len(istate); i++ {
-			dst = encoding.MarshalInt64(dst, istate[i].timestamp)
-			posbuffer = encoding.MarshalUint16(posbuffer, istate[i].pos)
-		}
-		dst = append(dst, posbuffer...)
-	}
-	return dst
-}
-
-func (idx *TokenIndex) marshalIdList(dst []byte, node *TrieNode) []byte {
-	dst = append(dst, txPrefixId)
-	ids := make([]uint32, len(node.ids))
-	for id := range node.ids {
-		ids = append(ids, id)
-	}
-	sort.Slice(ids, func(i, j int) bool {
-		return ids[i] < ids[j]
-	})
-
-	for i := 0; i < len(ids); i++ {
-		dst = encoding.MarshalUint32(dst, ids[i])
-	}
-
-	return dst
-}
-
-func (idx *TokenIndex) createIndex(vtoken string, flag uint8, node *TrieNode) error {
-	ii := idxItemsPool.Get()
-	defer idxItemsPool.Put(ii)
-
-	// write txPrefixPos + vtokens + suffix + flag
-	ii.B = append(ii.B, txPrefixPos)
-	ii.B = append(ii.B, []byte(vtoken)...)
-	ii.B = append(ii.B, txSuffix)
-	ii.B = append(ii.B, flag)
-
-	// write posList
-	if flag&posFlag != 0 {
-		ii.B = idx.marshalPosList(ii.B, node)
-	}
-
-	// write idlist
-	if flag&idFlag != 0 {
-		ii.B = idx.marshalIdList(ii.B, node)
-	}
-	ii.Next()
-	// write to mergeset
-	return idx.tb.AddItems(ii.Items)
-}
-
-func (idx *TokenIndex) writeDocumentIndex(vtoken string, node *TrieNode) {
-	var flag uint8
-	if len(node.invertedIndex) != 0 {
-		flag = flag | posFlag
-	}
-	if len(node.ids) != 0 {
-		flag = flag | idFlag
-	}
-	if flag != 0 {
-		idx.createIndex(vtoken, flag, node)
-		return
-	}
-	// traversal of trees.
-	for token, child := range node.children {
-		vtokens := vtoken + token + " "
-		idx.writeDocumentIndex(vtokens, child)
-	}
-}
-
-func (idx *TokenIndex) processDocument() {
-	// replace the root node
-	idx.trieLock.Lock()
-	if len(idx.root.children) == 0 {
-		idx.trieLock.Unlock()
-		return
-	}
-	node := idx.root
-	idx.docNum = 0
-	idx.root = NewTrieNode()
-	idx.trieLock.Unlock()
-
-	// Deal the first level node of the tree.
-	for token, child := range node.children {
-		vtokens := token + " "
-		idx.writeDocumentIndex(vtokens, child)
-	}
-
-}
-
-func (idx *TokenIndex) process() {
-	ticker := time.NewTicker(10 * time.Second)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-idx.closing:
-			return
-		case <-idx.signal:
-		case <-ticker.C:
-			idx.processDocument()
-		}
-	}
 }
 
 var clvSearchPool sync.Pool
